6f6d553786efd2906232d0e06ce9ce56aeca4886
BayShubar <yerkebulan199@gmail.com>
2017-10-21 19:30:02 +0600

    set up completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux'
+import Reducers from './store/Reducers'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux'
+
+
+const store = createStore(
+		Reducers,
+		composeWithDevTools(applyMiddleware(thunk))
+	);
+
+
+
+ReactDOM.render(
+	<Provider store = { store } >
+		<App />
+	</Provider>, 
+	document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store/reducers/UserReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+	id: 1,
+	name: 'Yerkebulan',
+	login: 'Yerke199'
+}
+
+export default function UserReducer(state = initialState, action){
+	return state;
+}
